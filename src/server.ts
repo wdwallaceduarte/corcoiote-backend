0ce2faa79f3670b7a3e6cdbccbeffee8806a8efd
wd.wallaceduarte <wd.wallaceduarte@gmail.com>
2026-07-17 21:41:39 -0300

    feat: estabelecer camada de servico para recurso de clientes

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,9 @@
 import express from "express";
-
+import CustomerRouter from './routes/customer.route.ts'
 const app = express()
 
 app.use(express.json())
 
-app.get('/customers', (request, response) => {
-    const customers = [{
-        name: 'Wallace Duarte',
-        status: true
-    }, {
-        name: 'Daniele',
-        status: true
-    }, {
-        name: 'Beatriz',
-        status: false
-    }]
-
-    response.status(200).json(customers)
-})
+app.use('/customers', CustomerRouter)
 
 app.listen(3000)
