5cb279690b2dd45ac71b7208a024e564558432d7
wd.wallaceduarte <wd.wallaceduarte@gmail.com>
2026-07-17 21:40:19 -0300

    refactor: mover dados simulados para diretorio dedicado

---
+++ src/mocks/customer.mock.ts
@@ -0,0 +1,17 @@
+import type { Customer } from '../types.ts'
+
+const customers: Customer [] = [{
+    id: 1,
+    name: 'Wallace Duarte',
+    status: true
+}, {
+    id: 2,
+    name: 'Daniele',
+    status: true
+}, {
+    id: 3,
+    name: 'Beatriz',
+    status: false
+}]
+
+export default customers
