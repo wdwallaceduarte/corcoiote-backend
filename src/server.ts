3298501f9be5b94ab9bc0d9b04ee5f554203eb44
wdwallaceduarte <antonioduarte0267@digitalcollege.internal>
2026-07-31 21:37:46 -0300

    feat: adicionar intermediarios para registro de requisicoes e manipulacao de erros

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import requestLogger from "./middlewares/requestLogger.ts";
 import CustomerRouter from './routes/customer.route.ts'
+import errorHandler from "./middlewares/errorHandler.ts";
+
 const app = express()
+
+app.use(requestLogger)
 
 app.use(express.json())
 
 app.use('/customers', CustomerRouter)
+
+app.use(errorHandler)
 
 app.listen(3000)
